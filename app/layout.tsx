9a08390f5ca4eb4c7fe6fcddc294c8b87f10c70b
sorious77 <sorious77@naver.com>
2024-08-07 21:37:12 +0900

    :art: prettier 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: "Homodoro",
-  description: "Manage your schedule with Homodoro",
+  title: 'Homodoro',
+  description: 'Manage your schedule with Homodoro',
 };
 
 export default function RootLayout({
@@ -12,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>{children}</body>
     </html>
   );
